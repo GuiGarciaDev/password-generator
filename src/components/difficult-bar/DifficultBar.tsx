361aafc1abbf3d8833af4c7dd93bdb933b8c59e2
GuilhermeGarcia <guilherme.garcia1136@gmail.com>
2023-02-08 15:55:29 -0300

    Password generator function
    General improvements
    Responsive layout
    Changes in DifficultBar component: New way to calculate how strong the password is
    Ready to deploy

--- src/components/difficult-bar/DifficultBar.tsx
+++ src/components/difficult-bar/DifficultBar.tsx
@@ -1,37 +1,41 @@
 import Rectangle from "../rectangle/Rectangle";
 
 interface Props {
-    value: number
+    charStrenght: number
+    length: number
 }
 
-export default function DifficultBar({ value } : Props) {
+export default function DifficultBar({ charStrenght, length } : Props) {
     let text;
-    switch (value) {
-        case 1:
-            text = 'WEAK'
-            break;
-        case 2:
-            text = 'WEAK'
-            break;
-        case 3:
-            text = 'MEDIUM'
-            break;
-        case 4:
-            text = 'STRONG'
-            break;
-    
-        default:
-            text = 'WEAK'
-            break;
+    let value = 0;
+    let passwordStrenght = charStrenght * length;
+
+    // VERY WEAK: 0 <= X <= 10 | WEAK: 10 <= X <= 20 | MEDIUM: 20 <= X <= 30 | STRONG: 30 <= X 
+
+    if (passwordStrenght > 0 && passwordStrenght <= 10) {
+        text = 'VERY WEAK'
+        value = 1
+    }
+    if (passwordStrenght > 10 && passwordStrenght <= 20) {
+        text = 'WEAK'
+        value = 2
+    }
+    if (passwordStrenght > 20 && passwordStrenght <= 30) {
+        text = 'MEDIUM'
+        value = 3
+    }
+    if (passwordStrenght > 30) {
+        text = 'STRONG'
+        value = 4
     }
 
     return (
         <div>
             {text}
-            <Rectangle active={value > 0}/>
-            <Rectangle active={value > 1}/>
-            <Rectangle active={value > 2}/>
-            <Rectangle active={value > 3}/>
+            <Rectangle active={value >= 1}/>
+            <Rectangle active={value >= 2}/>
+            <Rectangle active={value >= 3}/>
+            <Rectangle active={value >= 4}/>
         </div>
         
     )
